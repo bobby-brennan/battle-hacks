33b48f85e904f6808181d74a68b2d88a7a57d9d7
EC2 Default User <ec2-user@ip-172-31-41-62.us-west-2.compute.internal>
2014-08-10 00:19:03 +0000

    Moar changes

--- server.js
+++ server.js
@@ -32,18 +32,15 @@ var QUERY_SET_NAME = "UPDATE users SET name=?, lastPrompt=? where phoneNumber=?"
 var QUERY_SET_CHAR_NAME = "UPDATE users SET charName=?, lastPrompt=? where phoneNumber=?";
 var QUERY_LAST_PROMPT = "SELECT lastPrompt FROM users where phoneNumber=?";
 var QUERY_SET_LAST_PROMPT = "UPDATE users SET lastPrompt=? WHERE phoneNumber=?";
-var QUERY_GET_CHAR_DATA = "SELECT * FROM users where phoneNumber=?";
-var QUERY_UPDATE_TIME_SENSITVE_ITEMS = "UPDATE users SET charSavings=?, lastUpdate=CURDATE()"
+var QUERY_GET_CHAR_DATA = "SELECT *, CURDATE() - lastUpdate as timeDiff FROM users where phoneNumber=?";
+var QUERY_UPDATE_TIME_SENSITIVE_DATA = "UPDATE users SET charSavings=?, charItems=?, lastUpdate=CURDATE()";
+var QUERY_SET_ITEMS = "UPDATE users SET charItems=? where phoneNumber=?";
+var QUERY_GET_ITEMS = "SELECT charItems FROM users where phoneNumber=?";
 
 var app = express();
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs');
 app.use(BODY_PARSER.json());
-
-app.get("/res/*", function(req, res) {
-  console.log("sending: " + req.url);
-  res.sendFile("/home/ec2-user/git/battle-hacks" + req.url);
-});
 
 app.get("/", function (req, res) {
   res.render('index');
@@ -73,29 +70,78 @@ app.get("/incomingSms", function(req, res) {
 app.get("/character", function(req, res) {
   var number = URL_PARSER.parse(req.url, true).query.phoneNumber;
   getCharData(number, function(data) {
+    data['number'] = number;
     res.render('character', data);
   });
 });
 
-app.listen(80);
+app.post("/purchaseItem", function(req, res) {
+  var number = req.body.number;
+  var itemId = req.body.itemId;
+  console.log("purchasing:" + itemId);
+  purchaseItem(number, itemId);
+});
+
+app.listen(8000);
+
+var purchaseItem = function(number, itemId) {
+  DATABASE.query(QUERY_GET_ITEMS, [number], function (err, rows) {
+    if (err || rows.length != 1) {
+      console.log("error getting items for:" + number);
+      return;
+    }
+    console.log("inserting: " + itemId + " into " + rows[0].charItems);
+    if (rows[0].charItems) console.log("exists");
+    var oldItems = rows[0].charItems ? JSON.parse(rows[0].charItems) : {};
+    oldItems[itemId] = 100;
+    var newItems = JSON.stringify(oldItems);
+    DATABASE.query(QUERY_SET_ITEMS, [newItems, number], function (err) {
+      if (err) {
+        console.log("error setting items for:" + number + "  " + err);
+      }
+    });
+  });
+}
 
 var getCharData = function(number, callback) {
-  var number = URL_PARSER.parse(req.url, true).query.phoneNumber;
   DATABASE.query(QUERY_GET_CHAR_DATA, [number], function(err, rows) {
     if (err || rows.length != 1) {
       console.log("no char found for:" + number);
-      res.render('error', {message: "Character not found"});
-      return;
+      callback({});
     }
+    var charData = updateTimeSensitiveData(rows[0], number);
     charData = {
-      name: rows[0].charName,
-      income: rows[0].charDailyIncome,
-      savings: rows[0].charSavings,
-      health: rows[0].charHealth,
-      transportation: rows[0].charTransporation,
+      name: charData.charName,
+      income: charData.charDailyIncome,
+      savings: charData.charSavings,
+      health: charData.charHealth,
+      transportation: charData.charTransporation,
+      occupation: charData.charOccupation,
+      costOfLiving: charData.charCostOfLivingDaily,
+      items: charData.charItems
     }
     callback(charData);
   });
+}
+
+var updateTimeSensitiveData = function(oldData, number) {
+  if (oldData.timeDiff < 1) {
+    // Nothing for now.
+  }
+  items = JSON.parse(oldData.charItems);
+  for (var key in items) {
+    items[key] = Math.max(items[key] - 1, 0);
+  }
+  var newItems = JSON.stringify(items);
+  var newSavings = oldData.charSavings + 1;
+  oldData.charItems = newItems;
+  oldData.charSavings = newSavings;
+  DATABASE.query(QUERY_UPDATE_TIME_SENSITIVE_DATA, [newSavings, newItems, number], function(err) {
+    if(err) {
+      console.log("error updating time sensitive info");
+    }
+  });
+  return oldData;
 }
 
 var createUser = function(number) {
